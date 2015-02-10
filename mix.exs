dd9e387e725f147d100bbda1a1d3dfc65bad0e14
Jamie Winsor <jamie@vialstudios.com>
2015-02-10 11:35:04 -0800

    version bump 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Consul.Mixfile do
   def project do
     [
       app: :consul,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.0",
       deps: deps,
       package: package,
